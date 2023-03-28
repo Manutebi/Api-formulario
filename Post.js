c7df1cc1240abc334d51533b3589e4e6b5483c13
manutebi <ma.aninir@duocuc.cl>
2023-03-28 18:36:01 -0300

    Post tablas vinculadas, falta subir archivos

--- Post.js
+++ Post.js
@@ -3,47 +3,70 @@ const baseId = 'appU7lYsFSoNH4hGO';
 const tableName = 'Productos';
 
 const form = document.getElementById('product-form');
-
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-
+    
     const nombre = document.getElementById('nombre').value;
     const descripcion = document.getElementById('descripcion').value;
     const precio = parseFloat(document.getElementById('precio').value);
     const opciones = document.getElementById('Opciones').value;
-    // const table2 = document.getElementById('table2').value;
-    // const name = document.getElementById('name').value;
-    // const name2 = document.getElementById('name2').value;
-    // const assignee = document.getElementById('assignee').value;
-    // const notes = document.getElementById('notes').value;
-    // const attachment= document.getElementById(attachment).value;
+    const name2 = document.getElementById('name2').value;
+    const notes = document.getElementById('notes').value;
 
-    fetch(`https://api.airtable.com/v0/${baseId}/${tableName}`, {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${apiKey}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            fields: {
-                Nombre: nombre,
-                Descripcion: descripcion,
-                Precio: parseFloat(precio),
-                Opciones: opciones,
-                // Table2: [table2],
-                // Name: name,
-                // Name2: name2,
-                // Assignee: assignee,
-                // Notes: notes,
-                // Attachment: attachment
-            },
-        }),
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => {
-            console.log(error);
+    // Obtener el archivo del input
+    const fileInput = document.getElementById('file-input');
+    const file = fileInput.files[0];
+
+    // Crear una referencia al archivo en Firebase
+    const storageRef = firebase.storage().ref();
+    const fileRef = storageRef.child(file.name);
+
+    // Subir el archivo a Firebase
+    fileRef.put(file).then((snapshot) => {
+        // Obtener la URL pública del archivo subido
+        fileRef.getDownloadURL().then((url) => {
+            // Crear un nuevo registro en la tabla table_2
+            fetch(`https://api.airtable.com/v0/${baseId}/table_2`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${apiKey}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    fields: {
+                        Name2: name2,
+                        Notes: notes,
+                    },
+                }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    // Obtener el ID del registro creado
+                    const idCreado = data.id;
+
+                    // Usar el ID del registro creado para crear un nuevo registro en la tabla Productos y vincularlo al registro creado en la tabla table_2
+                    fetch(`https://api.airtable.com/v0/${baseId}/${tableName}`, {
+                        method: 'POST',
+                        headers: {
+                            'Authorization': `Bearer ${apiKey}`,
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            fields: {
+                                Nombre: nombre,
+                                Descripcion: descripcion,
+                                Precio: parseFloat(precio),
+                                Opciones: opciones,
+                                Table2: [idCreado],
+                                Attachment: [{ url }],
+                            },
+                        }),
+                    })
+                        .then((response) => response.json())
+                        .then((data) => {
+                            console.log(data);
+                        });
+                });
         });
+    });
 });
